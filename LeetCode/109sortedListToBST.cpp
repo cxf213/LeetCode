753f9ce5ca4869450f7104c3b0b5c6d24e57617b
cxf213 <cxf213@outlook.com>
2021-01-27 14:54:42 +0800

    109 bfs+dfs

---
+++ LeetCode/109sortedListToBST.cpp
@@ -0,0 +1,61 @@
+﻿#include <queue>
+using namespace std;
+
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+   struct TreeNode {
+       int val;
+       TreeNode *left;
+       TreeNode *right;
+       TreeNode() : val(0), left(nullptr), right(nullptr) {}
+       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+   };
+
+
+class Solution {
+    void dfs(ListNode*& head, TreeNode* root) {
+        if (root == NULL)  return;
+        dfs(head, root->left);
+        root->val = head->val;
+        head = head->next;
+        dfs(head, root->right);
+    }
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (head == NULL)
+            return NULL;
+        queue<TreeNode*> que;
+        ListNode* node = head;
+        TreeNode* root = new TreeNode();
+        que.push(root);
+        node = node->next;
+        while (node) {
+            TreeNode* temp = que.front();
+            que.pop();
+            temp->left = new TreeNode();
+            que.push(temp->left);
+            node = node->next;
+
+            if (!node)   break;
+            temp->right = new TreeNode();
+            que.push(temp->right);
+            node = node->next;
+        }
+        dfs(head, root);
+        return root;
+    }
+};
+
+//给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+//
+//本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+//https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/
+//深度优先搜索
